9f614e12a357aa0423ce5368f67e334c9f30813e
WhiteKIM <9rudrb40@naver.com>
2024-04-01 11:36:25 +0900

    init project

---
+++ src/main/java/hwagae/psp/PspApplication.java
@@ -0,0 +1,13 @@
+package hwagae.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hwagae/psp/PspApplicationTests.java
@@ -0,0 +1,13 @@
+package hwagae.psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
